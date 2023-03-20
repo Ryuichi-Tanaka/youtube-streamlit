029a5ff0cc4b86c70a2b3e2657b2482fdbed49a3
ryuichitanaka <tuutiyou.otameshi@gmail.com>
2023-03-20 17:50:12 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+st.title('streamlit 超入門')
+
+st.write('Display Image')
+'start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+
+for i in range(100):
+    latest_iteration.text(f'Iteration{i+1}')
+    bar.progress(i+1)
+    time.sleep(0.3)
+
+left_column,right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラムです')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせを書く')
+
+#text = st.text_input('あなたの趣味を教えてください。')
+#condition = st.slider('あなたの今の調子は？',0,100,50)
+
+#'あなたの趣味：',text,'です。'
+#'コンディション：',condition,'です。'
+
+#if st.checkbox('show image'):
+#    img = Image.open('ミッドサマー.jpg')
+#    st.image(img, caption='middo',use_column_width=True)
